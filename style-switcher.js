42f4378e51148d690cbaf32855bf8cdc391f3330
Yin Nu Nu Kyi <85919231+YinNuNuKyi@users.noreply.github.com>
2022-12-13 10:35:34 +0630

    Add files via upload

---
+++ style-switcher.js
@@ -0,0 +1,37 @@
+
+
+
+var styleSwitcherToggle=document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click",()=>{
+     document.querySelector(".style-switcher").classList.toggle(".open");
+     
+})
+
+window.addEventListener("scroll",()=>{
+     if( document.querySelector(".style-switcher").classList.contains("open")){
+          document.querySelector(".style-switcher").classList.remove("open");
+     }
+})
+
+var alternateStyle=document.querySelector(".alternate-style");
+function setActiveStyle(color){
+     alternateStyle.forEach((style) =>{
+          if(color==style.getAttribute("title")){
+               style.removeAttribute("disabled");
+          }
+           else{
+               style.setAttribute("disabled","true");
+           }
+     })
+} 
+
+const dayNight=document.querySelector("day-night");
+
+window.addEventListener("load",()=>{
+     if(document.body.classList.contains("dark")){
+          dayNight.querySelector("i").classList.add("fa-sun");
+     }
+      else{
+          dayNight.querySelector("i").classList.add("fa-moon");
+      }
+})
